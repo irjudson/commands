30faa6f282158bfe149b5c86a562031d9b094334
Tim Park <timfpark@gmail.com>
2013-11-08 23:07:33 -0800

    initial commit for camera

--- camera/test/units/cameraManager.js
+++ camera/test/units/cameraManager.js
@@ -1,6 +1,8 @@
 var assert = require('assert')
+  , config = require('../config')
   , nitrogen = require('nitrogen')
-  , CameraManager = require('../../manager');
+  , path = require('path')
+  , CameraManager = require('../../manager').CameraManager;
 
 describe('cameraManager', function() {
 
@@ -59,51 +61,4 @@ describe('cameraManager', function() {
             });
         });
     });
-
-    it('should detect motion', function(done) {
-        var cameraManager = new CameraManager();
-
-        var command = new nitrogen.Message({
-            id: '1',
-            type: 'cameraCommand',
-            expires: new Date(2050,1,1,0,0,0),
-            body: {
-                command: 'motion'
-            }
-        });
-
-        cameraManager.history = [
-            { path: path.join(__dirname, '../fixtures/images/motion0.jpg') },
-            { path: path.join(__dirname, '../fixtures/images/motion2.jpg') }
-        ];
-
-        cameraManager.detectMotion(command, function(motion) {
-            assert.equal(motion, true);
-            done();
-        });
-    });
-
-    it('should not detect motion with static images', function(done) {
-        var cameraManager = new CameraManager();
-
-        var command = new nitrogen.Message({
-            id: '1',
-            type: 'cameraCommand',
-            expires: new Date(2050,1,1,0,0,0),
-            body: {
-                command: 'motion'
-            }
-        });
-
-        cameraManager.history = [
-            { path: path.join(__dirname, '../fixtures/images/motion0.jpg') },
-            { path: path.join(__dirname, '../fixtures/images/motion0.jpg') }
-        ];
-
-        cameraManager.detectMotion(command, function(motion) {
-            assert.equal(motion, false);
-            done();
-        });
-    });
-
 });
