c71143be91536bbe5d82a7292bcca5bd089c2cef
Tim Park <timfpark@gmail.com>
2014-01-21 06:14:46 -0800

    reduce message stream to current state

--- light/test/units/lightManager.js
+++ light/test/units/lightManager.js
@@ -23,4 +23,37 @@ describe('lightManager', function() {
         });
     });
 
+    it('should be able to combine state and commands to reach the right final state', function(done) {
+        var lightManager = new LightManager();
+        lightManager.process(
+            new nitrogen.Message({
+                id: '1',
+                type: 'lightState',
+                expires: new Date(2050,1,1,0,0,0),
+                body: {
+                    on: true,
+                    hue: 255
+                }
+            })
+        );
+
+        lightManager.applyCommand(
+            new nitrogen.Message({
+                id: '1',
+                type: 'lightCommand',
+                expires: new Date(2050,1,1,0,0,0),
+                body: {
+                    sat: 100,
+                    hue: 150
+                }
+            })
+        );
+
+        assert.equal(lightManager.state.sat, 100);
+        assert.equal(lightManager.state.hue, 150);
+        assert.equal(lightManager.state.on, true);
+
+        done();
+    });
+
 });
