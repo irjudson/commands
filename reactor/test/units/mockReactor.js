ba6c8d80103d86e0da9cbbaf7bf8c613a2d73d50
Tim Park <timfpark@gmail.com>
2014-03-03 18:19:40 -0800

    update reactor manager to match new reactor interface

--- reactor/test/units/mockReactor.js
+++ reactor/test/units/mockReactor.js
@@ -2,6 +2,10 @@ function MockReactor() {
     this.state = null;
 }
 
+MockReactor.prototype.shutdown = function(command) {
+    this.state = 'stopped';
+};
+
 MockReactor.prototype.start = function(command) {
     this.state = 'started';
 };
