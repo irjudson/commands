582ebadb0ad9698aa1012ba5bad335483cc2550c
Tim Park <timfpark@gmail.com>
2014-03-04 19:51:20 -0800

    test reactorManager with basic unit tests

--- reactor/test/units/reactorManager.js
+++ reactor/test/units/reactorManager.js
@@ -5,62 +5,109 @@ var assert = require('assert')
   , nitrogen = require('nitrogen');
 
 describe('reactorManager', function() {
+    var reactor = new MockReactor({ nickname: 'mockreactor' });
+    var reactorManager = new ReactorManager(reactor);
 
-    var messageQueueFixture1 = [{ 
-        id: '1', 
-        ts: new Date(), 
-        type: 'reactorCommand', 
-        body: { 
-            command: 'start',
-            instance_id: '1AZ' 
-        } 
-    }];
+    var installFixture = [new nitrogen.Message({ 
+            id: '1', 
+            ts: new Date(), 
+            type: 'reactorCommand', 
+            body: { 
+                command: 'install',
+                module: 'nitrogen-test-app',
+                config: {},
+                instance_id: '1AZ' 
+            } 
+        })
+    ];
 
-    var messageQueueFixture2 = [{ 
-        id: '1', 
-        ts: new Date(), 
-        type: 'reactorCommand', 
-        body: { 
-            command: 'start', 
-            instance_id: '1AZ' 
-        } 
-    }, { 
-        id: '2', 
-        ts: new Date(), 
-        type: 'reactorState',   
-        body: { 
-            state: 'started', 
-            instance_id: '1AZ' 
-        } 
-    }, { 
-        id: '3', 
-        ts: new Date(), 
-        type: 'reactorCommand', 
-        body: { 
-            command: 'stop',
-            instance_id: '1AZ' 
-        } 
-    }];
-
-    function loadFixture(reactorManager, fixture) {
-        reactorManager.messageQueue = fixture.map(function(obj) {
-            return new nitrogen.Message(obj);
-        });
-    }
-
-    it('should be able to process message stream with start to current state', function(done) {
-        var service = new nitrogen.Service(config);
-
-        var reactor = new MockReactor();
-        var reactorManager = new ReactorManager(reactor);
-
-        loadFixture(reactorManager, messageQueueFixture1);
-
+    it('should process install message', function(done) {
+        installFixture.forEach(function(message) { reactorManager.process(message); });
         reactorManager.executeQueue();
 
-        assert.equal(reactorManager.reactorState['1AZ'].command.body.command, 'start');
-        assert.equal(reactor.state, 'started');
+        setTimeout(function() {
+            var state = reactorManager.device.getState();
 
-        done();
+            assert(state['1AZ'].command, installFixture[0]);
+            assert(state['1AZ'].state, 'stopped');
+
+            done();
+        }, 300);
     });
+
+    var startFixture = [new nitrogen.Message({ 
+            id: '2', 
+            ts: new Date(), 
+            type: 'reactorCommand', 
+            body: { 
+                command: 'start',
+                instance_id: '1AZ' 
+            } 
+        })
+    ];
+
+    it('should process start message', function(done) {
+        startFixture.forEach(function(message) { reactorManager.process(message); });
+        reactorManager.executeQueue();
+
+        setTimeout(function() {
+            var state = reactorManager.device.getState();
+
+            assert(state['1AZ'].command, startFixture[0]);
+            assert(state['1AZ'].state, 'running');
+
+            done();
+        }, 300);
+    });
+
+    var stopFixture = [new nitrogen.Message({ 
+            id: '3', 
+            ts: new Date(), 
+            type: 'reactorCommand', 
+            body: { 
+                command: 'stop',
+                instance_id: '1AZ' 
+            } 
+        })
+    ];
+
+    it('should process stop message', function(done) {
+        stopFixture.forEach(function(message) { reactorManager.process(message); });
+        reactorManager.executeQueue();
+
+        setTimeout(function() {
+            var state = reactorManager.device.getState();
+
+            assert(state['1AZ'].command, stopFixture[0]);
+            assert(state['1AZ'].state, 'stopped');
+
+            done();
+        }, 300);
+    });
+
+    var uninstallFixture = [new nitrogen.Message({ 
+            id: '3', 
+            ts: new Date(), 
+            type: 'reactorCommand', 
+            body: { 
+                command: 'uninstall',
+                instance_id: '1AZ' 
+            } 
+        })
+    ];
+
+    it('should process uninstall message', function(done) {
+        uninstallFixture.forEach(function(message) { reactorManager.process(message); });
+        reactorManager.executeQueue();
+
+        setTimeout(function() {
+            var state = reactorManager.device.getState();
+
+            assert(state['1AZ'].command, uninstallFixture[0]);
+            assert(state['1AZ'].state, 'uninstalled');
+
+            done();
+        }, 300);
+    });
+
 });
