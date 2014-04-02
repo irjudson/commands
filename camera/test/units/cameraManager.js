0c322831f360eb1c1b33379632c69d178a2554b9
Tim Park <timfpark@gmail.com>
2014-04-01 18:23:13 -0700

    improve filter for CameraManager

--- camera/test/units/cameraManager.js
+++ camera/test/units/cameraManager.js
@@ -5,42 +5,6 @@ var assert = require('assert')
   , CameraManager = require('../../manager');
 
 describe('cameraManager', function() {
-
-    it('should be able to calculate the right amount of history required', function(done) {
-        var cameraManager = new CameraManager();
-        cameraManager.messageQueue = [
-            new nitrogen.Message({
-                id: '1',
-                type: 'cameraCommand',
-                expires: new Date(2050,1,1,0,0,0),
-                body: {
-                    command: 'motion'
-                }
-            })
-        ];
-
-        assert.equal(cameraManager.historyRequired(), 3);
-
-        cameraManager.messageQueue.push(new nitrogen.Message({
-            id: '2',
-            type: 'cameraCommand',
-            expires: new Date(2050,1,1,0,0,0),
-            body: {
-                command: 'snapshot'
-            }
-        }));
-
-        assert.equal(cameraManager.historyRequired(), 3);
-
-        cameraManager.messageQueue = cameraManager.messageQueue.slice(1);
-        assert.equal(cameraManager.historyRequired(), 1);
-
-        cameraManager.messageQueue = [];
-        assert.equal(cameraManager.historyRequired(), 0);
-
-        done();
-    });
-
     it('should be able to start a CameraManager with a device', function(done) {
         var service = new nitrogen.Service(config);
 
